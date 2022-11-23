2ce6e935aa80e400a8c0d782d42231a6507976cc
KWJYES <iwkj@foxmail.com>
2022-11-24 01:11:38 +0800

    写完了手机号判断函数，并写了一点简单的Test

--- src/main/java/PhoneNumJudge.java
+++ src/main/java/PhoneNumJudge.java
@@ -5,76 +5,74 @@ import java.util.regex.Pattern;
 
 public class PhoneNumJudge {
     //存放三大运营商号码的前3位——网络识别号
-    private final Map<String,String> networkIdNumMap;
+    private final Map<String, String> networkIdNumMap;
 
     public PhoneNumJudge() {
         networkIdNumMap = new HashMap<>();
         //中国电信 133、149、153、173、177、180、181、189、191、199
-        networkIdNumMap.put("133","中国电信");
-        networkIdNumMap.put("149","中国电信");
-        networkIdNumMap.put("153","中国电信");
-        networkIdNumMap.put("173","中国电信");
-        networkIdNumMap.put("177","中国电信");
-        networkIdNumMap.put("180","中国电信");
-        networkIdNumMap.put("181","中国电信");
-        networkIdNumMap.put("189","中国电信");
-        networkIdNumMap.put("191","中国电信");
-        networkIdNumMap.put("199","中国电信");
+        networkIdNumMap.put("133", "中国电信");
+        networkIdNumMap.put("149", "中国电信");
+        networkIdNumMap.put("153", "中国电信");
+        networkIdNumMap.put("173", "中国电信");
+        networkIdNumMap.put("177", "中国电信");
+        networkIdNumMap.put("180", "中国电信");
+        networkIdNumMap.put("181", "中国电信");
+        networkIdNumMap.put("189", "中国电信");
+        networkIdNumMap.put("191", "中国电信");
+        networkIdNumMap.put("199", "中国电信");
         //中国联通 130、131、132、145、155、156、166、171、175、176、185、186
-        networkIdNumMap.put("130","中国联通");
-        networkIdNumMap.put("131","中国联通");
-        networkIdNumMap.put("132","中国联通");
-        networkIdNumMap.put("145","中国联通");
-        networkIdNumMap.put("155","中国联通");
-        networkIdNumMap.put("156","中国联通");
-        networkIdNumMap.put("166","中国联通");
-        networkIdNumMap.put("171","中国联通");
-        networkIdNumMap.put("175","中国联通");
-        networkIdNumMap.put("176","中国联通");
-        networkIdNumMap.put("185","中国联通");
-        networkIdNumMap.put("186","中国联通");
+        networkIdNumMap.put("130", "中国联通");
+        networkIdNumMap.put("131", "中国联通");
+        networkIdNumMap.put("132", "中国联通");
+        networkIdNumMap.put("145", "中国联通");
+        networkIdNumMap.put("155", "中国联通");
+        networkIdNumMap.put("156", "中国联通");
+        networkIdNumMap.put("166", "中国联通");
+        networkIdNumMap.put("171", "中国联通");
+        networkIdNumMap.put("175", "中国联通");
+        networkIdNumMap.put("176", "中国联通");
+        networkIdNumMap.put("185", "中国联通");
+        networkIdNumMap.put("186", "中国联通");
         //中国移动 134、135、136、137、138、139、147、150、151、152、157、158、159、182、183、187、188、178
-        networkIdNumMap.put("134","中国移动");
-        networkIdNumMap.put("135","中国移动");
-        networkIdNumMap.put("136","中国移动");
-        networkIdNumMap.put("137","中国移动");
-        networkIdNumMap.put("138","中国移动");
-        networkIdNumMap.put("139","中国移动");
-        networkIdNumMap.put("147","中国移动");
-        networkIdNumMap.put("150","中国移动");
-        networkIdNumMap.put("151","中国移动");
-        networkIdNumMap.put("152","中国移动");
-        networkIdNumMap.put("157","中国移动");
-        networkIdNumMap.put("158","中国移动");
-        networkIdNumMap.put("159","中国移动");
-        networkIdNumMap.put("182","中国移动");
-        networkIdNumMap.put("183","中国移动");
-        networkIdNumMap.put("187","中国移动");
-        networkIdNumMap.put("188","中国移动");
-        networkIdNumMap.put("178","中国移动");
-    }
-
-    public String judgePhoneNum(String phoneNum){
-        if(!isNumeric(phoneNum))
-            return "输入不合法";
-        if(phoneNum.length()!=11)
-            return "手机号不正确";
-        if(networkIdNumMap.containsKey(phoneNum.substring(0,3))){
-            return "手机号正确";
-        }else {
-            return "手机号不正确";
-        }
+        networkIdNumMap.put("134", "中国移动");
+        networkIdNumMap.put("135", "中国移动");
+        networkIdNumMap.put("136", "中国移动");
+        networkIdNumMap.put("137", "中国移动");
+        networkIdNumMap.put("138", "中国移动");
+        networkIdNumMap.put("139", "中国移动");
+        networkIdNumMap.put("147", "中国移动");
+        networkIdNumMap.put("150", "中国移动");
+        networkIdNumMap.put("151", "中国移动");
+        networkIdNumMap.put("152", "中国移动");
+        networkIdNumMap.put("157", "中国移动");
+        networkIdNumMap.put("158", "中国移动");
+        networkIdNumMap.put("159", "中国移动");
+        networkIdNumMap.put("182", "中国移动");
+        networkIdNumMap.put("183", "中国移动");
+        networkIdNumMap.put("187", "中国移动");
+        networkIdNumMap.put("188", "中国移动");
+        networkIdNumMap.put("178", "中国移动");
     }
 
     /**
-     * 利用正则表达式判断字符串是否是数字
-     * @param str
-     * @return
+     * 输入一个11位数字的字符串，
+     * 判断它是不是一个手机号码，
+     * 如果是返回该手机号的所属的“运营商名称”
+     * 如果不是返回“不是一个手机号”
+     *
+     * @param phoneNum 输入的字符串
+     * @return 判断结果
      */
-    public boolean isNumeric(String str){
-        Pattern pattern = Pattern.compile("[0-9]*");
-        Matcher isNum = pattern.matcher(str);
-        return isNum.matches();
+    public String judgePhoneNum(String phoneNum) {
+        for (int i = 0; i < phoneNum.length(); i++) {
+            if (phoneNum.charAt(i) < '0' || phoneNum.charAt(i) > '9')
+                return "输入不合法";
+        }
+        if (phoneNum.length() == 11 && networkIdNumMap.containsKey(phoneNum.substring(0, 3))) {
+            return networkIdNumMap.get(phoneNum.substring(0, 3));
+        } else {
+            return "不是一个手机号";
+        }
     }
 
 }
--- src/test/java/PhoneNumTest.java
+++ src/test/java/PhoneNumTest.java
@@ -1,6 +1,44 @@
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PhoneNumTest {
+
+    private PhoneNumJudge phoneNumJudge;
+
+    @Before
+    public void init() {
+        phoneNumJudge = new PhoneNumJudge();
+    }
+
+    /**
+     * 11位数字是手机号测试
+     */
     @Test
-    public void
+    public void positiveTest() {
+        Assert.assertEquals("中国电信",phoneNumJudge.judgePhoneNum("19167867643"));
+        Assert.assertEquals("中国联通",phoneNumJudge.judgePhoneNum("13057186923"));
+        Assert.assertEquals("中国移动",phoneNumJudge.judgePhoneNum("13457186923"));
+    }
+
+
+    /**
+     * 非法输入测试
+     */
+    @Test
+    public void nullInputTest(){
+        Assert.assertEquals("输入不合法",phoneNumJudge.judgePhoneNum(""));
+    }
+
+    @Test
+    public void lengthIllegalInputTest(){
+        Assert.assertEquals("输入不合法",phoneNumJudge.judgePhoneNum("123"));
+
+    }
+
+    @Test
+    public void notNumInputTest(){
+        Assert.assertEquals("输入不合法",phoneNumJudge.judgePhoneNum("134*57935509"));
+        Assert.assertEquals("输入不合法",phoneNumJudge.judgePhoneNum("191*579"));
+    }
 }
